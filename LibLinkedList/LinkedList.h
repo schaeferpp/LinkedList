763a2739253f9130affe3aac972a55c5028ab494
schaeferpp <schaefer.pp@gmail.com>
2013-03-21 21:07:58 +0100

    documentation added

--- LibLinkedList/LinkedList.h
+++ LibLinkedList/LinkedList.h
@@ -1,6 +1,9 @@
 #ifndef __LINKEDLIST_H
 #define __LINKEDLIST_H
 	
+/**
+* Represents the data node. Pointer to following and previous node are stored.
+*/
 struct node
 {
 	struct node* next;
@@ -12,6 +15,11 @@ typedef struct node node_t;
 
 typedef node_t* pnode;
 
+/**
+ * Represents the actual list. Three pointers are defined: head, last and a runner.
+ * runnerpos defines the current index of the node* runner.
+ * size defines the current size of the list.
+ */
 struct linked_list
 {
 	pnode head;
@@ -25,16 +33,35 @@ typedef struct linked_list linked_list_t;
 
 typedef linked_list_t* plinked_list;
 
+/**
+ * Appends value to the end of the list.
+ */
 void list_append(plinked_list list, void* value);
 
+/**
+ * Inserts value into the list at the specified position. The current object at position
+ * will be at (position+1) after this operation.
+ */
 void list_insert(plinked_list list, int position, void* value);
 
+/**
+ * Removes the element of the list at the specified position.
+ */
 void list_remove(plinked_list list, int position);
 
+/**
+ * Returns a pointer to the object at position.
+ */
 void* list_get(plinked_list list, int position);
 
+/**
+ * Inserts all elements of elems into the list. count has to be defined.
+ */
 void list_add_all(plinked_list list, void** elems, int count);
 
+/**
+ * Initializes an empty list. Works like an constructor.
+ */
 plinked_list init_list();
 
 #endif
